49df66b7ff78d169c42af1afdafa80bb4342622d
open7rms <open7rms@gmail.com>
2022-01-29 20:37:12 +0900

    update

--- 2022.1.28_python/BJ_12873.py
+++ 2022.1.28_python/BJ_12873.py
@@ -1,13 +1,13 @@
 n=int(input())
 round=[]
-for i in range(n):
+for i in range(1,n+1):
     round.append(i)
     
 print(round)
-count=1
-for i in range(n,1,-1) :
-
-    ceil(count**3//i)
+for i in range(1,n) :
+    #round.pop((i**3)%(n-i)-1)
+    print(round,round.pop((i**3)%(n-i+1)-1))
+#print(round)
 
 
 
