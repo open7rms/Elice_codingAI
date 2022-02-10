da746b491bf820a2813df318238c0939ed63ac50
open7rms <open7rms@gmail.com>
2022-02-10 23:15:50 +0900

    bineary search exercise

---
+++ 2022.1stTerm_3rdMt_python/BJ_2805.py
@@ -0,0 +1,32 @@
+from xmlrpc.client import _MultiCallMethod
+import sys
+
+#input
+pilen,need=map(int,sys.stdin.readline().split())
+piles=list(map(int,sys.stdin.readline().split()))
+
+#length difference calculation
+def diff(length,mid):
+    value=0
+    for i in range(len(length)):
+        if (length[i]-mid) >0 :
+            value +=length[i]-mid
+    return value
+#binery search engine
+low=1
+high=max(piles)
+while low<=high :
+    mid=(low+high)//2
+    calNeed=diff(piles,mid)
+    if calNeed==need :
+        print(mid)
+        break
+    elif calNeed > need:
+        low = mid+1
+        #print("high",mid,diff(piles,mid),low)
+    elif calNeed < need:
+        high =mid-1
+        #print("low",mid,diff(piles,mid),high)
+
+
+#print(pilen,need,piles)
