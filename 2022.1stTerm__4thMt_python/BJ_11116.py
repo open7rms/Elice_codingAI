7cba031a1b1f36e034921b93147794e6eb58b1f5
open7rms <open7rms@gmail.com>
2022-02-12 00:29:37 +0900

    update solution

--- 2022.1stTerm__4thMt_python/BJ_11116.py
+++ 2022.1stTerm__4thMt_python/BJ_11116.py
@@ -1,7 +1,16 @@
-round=int(input())
+round=int(input()) # # of test case
+def diff(a,b): #function diff 왼쪽으로 가는 차는 왼쪽 바퀴의 시간이 오른쪽 바퀴의 시간보다 이르다.
+    return a-b
+result =[] #출력할 결과값 list
+
 for i in range(round):
-    count=int(input())
-    x=list(map(int,input().split()))
-    y=list(map(int,input().split()))
-    print(x,y)
-    
+    count=int(input()) # # of time log
+    x=list(map(int,input().split())) # left tire time log
+    y=list(map(int,input().split())) # right tire time log
+    #print(x,y)
+    count =0
+    for i in list(map(diff,x,y)) :
+        if i <0 : count +=1 # 왼쪽으로 가는 차량의 # of time log
+    result.append(count)   
+for i in result : # print function
+    print(int(i/2))   # one car has two time-logs
