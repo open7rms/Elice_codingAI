3d9f29900e972c2970375ba9b44c80a8e4c5d87d
open7rms <open7rms@gmail.com>
2022-02-25 23:09:49 +0900

    update

--- 2022.1stTerm_5thMt_python/BJ_1874.py
+++ 2022.1stTerm_5thMt_python/BJ_1874.py
@@ -1,25 +1,21 @@
+
 nrlength=int(input())
 result=[]
-que1=[int(i) for i in range(1,nrlength)]
-que2=[]
-queanswer=[]
-que3=[]
-for i in range(nrlength):
-    result.append(int(input()))
-result.reverse()
-print(result)
-for i in que1:
-    if i ==result[i-1] :
-        que2.append(i)
-        queanswer.append("+push")
-        print(queanswer,que2)
+
+stack = []
+counter =1
+for i in range(nrlength) :
+    num = int(input())
+    while counter<=num :
+        stack.append(counter)
+        counter+=1
+        result.append('+')
+    if num ==stack[-1]:
+        stack.pop()
+        result.append('-')
     else :
-        #sub=result[i-1]-i
-        for j in range(i,result[i-1]) :
-            que2.append(i)
-            queanswer.append("-push")
-            print(queanswer,que2,j,result[i-1])
-        for j in range(i,result[i-1]) :
-            que3.append(que2.pop(-1))
-            queanswer.append("pop")        
-            print(queanswer,que3)
+        print('No')
+        exit(0)
+
+for i in result : 
+    print(i)
